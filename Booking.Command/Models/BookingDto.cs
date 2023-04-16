0bb2be94f748dfb28979cdf49644a447f2a3ae63
aussiearef <arefkr@gmail.com>
2023-04-17 07:09:48 +1000

    Booking-Command was pushed

---
+++ Booking.Command/Models/BookingDto.cs
@@ -0,0 +1,23 @@
+using Amazon.DynamoDBv2.DataModel;
+
+namespace Booking.Command.Models;
+
+[DynamoDBTable("Booking")]
+public class BookingDto
+{
+    public string? IdToken { get; set; }
+    public string? HotelId { get; set; }
+    public string? CheckinDate { get; set; }
+    public string? CheckoutDate { get; set; }
+
+    [DynamoDBHashKey("UserId")] public string? UserId { get; set; }
+
+    [DynamoDBRangeKey("Id")] public string? Id { get; set; }
+
+    public string? GivenName { get; set; }
+    public string? FamilyName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+
+    public BookingStatus Status { get; set; }
+}
---
+++ Booking.Command/Models/BookingStatus.cs
@@ -0,0 +1,8 @@
+namespace Booking.Command.Models;
+
+public enum BookingStatus
+{
+    Pending = 1,
+    Confirmed = 2,
+    Rejected = 3
+}
